16497df34e33887e3976f6b0bd94fcb4fe4ffb00
Evgeny <vasilkalin@gmail.com>
2021-10-10 12:18:38 +0000

    Doing for https://habr.com/ru/post/496182/

---
+++ src/main/java/com/example/MiniProjectKafka/MiniProjectKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.MiniProjectKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MiniProjectKafka/MiniProjectKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiniProjectKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
